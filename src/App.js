dde9880d06fcac7cdaae375754a93896dbd5564e
Rin <info@rin-m.site>
2021-08-29 11:18:47 +0900

    Add Search Box!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Accordion from './components/Accordion'
+import Search from './components/Search'
 
-function App() {
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React は、インタラクティブなユーザインターフェイスの作成にともなう苦痛を取り除きます。アプリケーションの各状態に対応するシンプルな View を設計するだけで、React はデータの変更を検知し、関連するコンポーネントだけを効率的に更新、描画します。'
+  },
+  {
+    title: 'How do we show content with React?',
+    content: '自分自身の状態を管理するカプセル化されたコンポーネントをまず作成し、これらを組み合わせることで複雑なユーザインターフェイスを構築します。'
+  },
+  {
+    title: 'Why is React useful for engineers?',
+    content: 'React と組み合わせて使用する技術に制限はありません。React を使って新しい機能を追加する際に、既存のソースコードを書き換える必要はありません。'
+  }
+]
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search />
     </div>
-  );
+  )
 }
-
-export default App;
